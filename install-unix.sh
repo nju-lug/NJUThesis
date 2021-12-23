9cf0b91c08e5b217cc0a24d37e21ff805501a961
dbxy <181850195@smail.nju.edu.cn>
2021-12-23 13:41:53 +0800

    feat: 新增插入外部校徽校名图片的方法 - 修改测试和发布脚本 - 修改默认模板 - 修改声明页选项名称

--- install-unix.sh
+++ install-unix.sh
@@ -6,15 +6,23 @@ cd "mythesis"
 cp -f "../docs/njuthesis-sample.tex"            .
 cp -f "../docs/njuthesis-sample.bib"            .
 cp -f "../source/njuthesis.dtx"                 .
+cp -f "../scripts/generate-img.tex"             .
 
 wget -q "https://mirror.nju.edu.cn/CTAN/macros/latex/contrib/njuvisual/njuvisual.dtx"
 wget -q "https://mirror.nju.edu.cn/CTAN/macros/latex/contrib/njuvisual/njuvisual-curves.dtx"
 
-xetex "njuthesis.dtx" > /dev/null
-xetex "njuvisual.dtx" > /dev/null
+xetex "njuthesis.dtx"                      > /dev/null
+xetex "njuvisual.dtx"                      > /dev/null
+pdflatex --shell-escape "generate-img.tex" > /dev/null
 
+rm *.aux
+rm *.auxlock
+rm *.dpth
 rm *.dtx
 rm *.ins
 rm *.log
+rm *.md5
+rm generate-img.tex
+rm generate-img.pdf
 
 cd ..
--- install-win.bat
+++ install-win.bat
@@ -6,15 +6,23 @@ CD    "mythesis"
 COPY /Y "..\docs\njuthesis-sample.tex"            .
 COPY /Y "..\docs\njuthesis-sample.bib"            .
 COPY /Y "..\source\njuthesis.dtx"                 .
+COPY /Y "..\scripts\generate-img.tex"             .
 
 curl -s -o "njuvisual.dtx" "https://mirror.nju.edu.cn/CTAN/macros/latex/contrib/njuvisual/njuvisual.dtx"
 curl -s -o "njuvisual-curves.dtx" "https://mirror.nju.edu.cn/CTAN/macros/latex/contrib/njuvisual/njuvisual-curves.dtx"
 
-xetex "njuthesis.dtx" > NUL
-xetex "njuvisual.dtx" > NUL
+xetex "njuthesis.dtx"                      > NUL
+xetex "njuvisual.dtx"                      > NUL
+pdflatex --shell-escape "generate-img.tex" > NUL
 
+DEL "*.aux"
+DEL "*.auxlock"
+DEL "*.dpth"
 DEL "*.dtx"
 DEL "*.ins"
 DEL "*.log"
+DEL "*.md5"
+DEL "generate-img.tex"
+DEL "generate-img.pdf"
 
 CD ..
