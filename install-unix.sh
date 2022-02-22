fef741062edb459e6385e6337e0c213a89d518b4
Yu Xiong <181850195@smail.nju.edu.cn>
2022-02-22 21:48:02 +0800

    refactor: 不再默认载入 njuvisual 宏包 - 简化安装脚本 - 在仓库内储存校名校徽图片

--- install-unix.sh
+++ install-unix.sh
@@ -5,25 +5,14 @@ cd "mythesis"
 
 cp -f "../docs/njuthesis-sample.tex"            .
 cp -f "../docs/njuthesis-sample.bib"            .
+cp -f "../docs/nju-emblem.pdf"                  .
+cp -f "../docs/nju-name.pdf"                    .
 cp -f "../source/njuthesis.dtx"                 .
-cp -f "../scripts/generate-img.tex"             .
 
-wget -q "https://mirror.nju.edu.cn/CTAN/macros/latex/contrib/njuvisual/njuvisual.dtx"
-wget -q "https://mirror.nju.edu.cn/CTAN/macros/latex/contrib/njuvisual/njuvisual-curves.dtx"
+xetex "njuthesis.dtx" > /dev/null
 
-xetex "njuthesis.dtx"                      > /dev/null
-xetex "njuvisual.dtx"                      > /dev/null
-pdflatex --shell-escape "generate-img.tex" > /dev/null
-
-rm *.aux
-rm *.auxlock
-rm *.dpth
 rm *.dtx
 rm *.ins
 rm *.log
-rm *.md5
-rm generate-img.tex
-rm generate-img.pdf
-rm njuvisual-example.tex
 
 cd ..
--- install-win.bat
+++ install-win.bat
@@ -5,25 +5,14 @@ CD    "mythesis"
 
 COPY /Y "..\docs\njuthesis-sample.tex"            .
 COPY /Y "..\docs\njuthesis-sample.bib"            .
+COPY /Y "..\docs\nju-emblem.pdf"                  .
+COPY /Y "..\docs\nju-name.pdf"                    .
 COPY /Y "..\source\njuthesis.dtx"                 .
-COPY /Y "..\scripts\generate-img.tex"             .
-
-curl -s -o "njuvisual.dtx" "https://mirror.nju.edu.cn/CTAN/macros/latex/contrib/njuvisual/njuvisual.dtx"
-curl -s -o "njuvisual-curves.dtx" "https://mirror.nju.edu.cn/CTAN/macros/latex/contrib/njuvisual/njuvisual-curves.dtx"
 
 xetex "njuthesis.dtx"                      > NUL
-xetex "njuvisual.dtx"                      > NUL
-pdflatex --shell-escape "generate-img.tex" > NUL
 
-DEL "*.aux"
-DEL "*.auxlock"
-DEL "*.dpth"
 DEL "*.dtx"
 DEL "*.ins"
 DEL "*.log"
-DEL "*.md5"
-DEL "generate-img.tex"
-DEL "generate-img.pdf"
-DEL "njuvisual-example.tex"
 
 CD ..
