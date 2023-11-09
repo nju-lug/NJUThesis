8467d95817dfe7408377fb9a4d5f5d23537389a6
atxy <y-xiong22@mails.tsinghua.edu.cn>
2023-11-10 00:25:53 +0800

    chore: 更正安装脚本

--- install-unix.sh
+++ install-unix.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env sh
 
-mkdir -p "mythesis"
-cd "mythesis"
+mkdir -p mythesis
+cd mythesis
 
-cp -f "../docs/njuthesis-sample.tex"            .
-cp -f "../docs/njuthesis-sample.bib"            .
-cp -f "../docs/njuthesis-setup.def"             .
-cp -f "../docs/nju-emblem*.pdf"                 .
-cp -f "../docs/nju-name*.pdf"                   .
-cp -f "../source/njuthesis.dtx"                 .
+cp -f ../docs/njuthesis-sample.tex            .
+cp -f ../docs/njuthesis-sample.bib            .
+cp -f ../docs/njuthesis-setup.def             .
+cp -f ../docs/nju-emblem*.pdf                 .
+cp -f ../docs/nju-name*.pdf                   .
+cp -f ../source/njuthesis.dtx                 .
 
-xetex "njuthesis.dtx" > /dev/null
+xetex njuthesis.dtx > /dev/null
 
 rm *.dtx
 rm *.ins
